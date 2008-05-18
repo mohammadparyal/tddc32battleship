6778e83d1e954eea0615b30cf5c62057d965a6b0
david.gunnarsson <david.gunnarsson@1a43c647-8f47-0410-a05c-a55b1d21bbad>
2008-05-18 22:53:12 +0000

    Funkar inte, men har ändrat lite färger och text

---
+++ Battleship/src/notWorking/battleship/Fleet.java
@@ -0,0 +1,66 @@
+package battleship;
+import java.util.*;
+
+import ships.Ship;
+
+/**
+ * Creates the ships and holds them in a list. It also contains methods for
+ * access to the ships and information about whether the fleet is placed or not 
+ * and sunk or not.
+ * 
+ * @author Lars Öberg and David Gunnarsson
+ *
+ */
+public class Fleet {
+
+	private ArrayList<Ship> ships;
+	private Game game;
+	
+	//Constructor	
+	public Fleet(Game game){
+		this.game = game;
+		ships = new ArrayList<Ship>();
+		ships.add(new Ship(5, "Aircraft Carrier", this));
+		ships.add(new Ship(4, "Battleship", this));
+		ships.add(new Ship(3, "Destroyer", this));
+		ships.add(new Ship(3, "Submarine", this));
+		ships.add(new Ship(2, "Patrol Boat", this));						
+		
+	}
+	
+	//Methods
+	public boolean shipLeft(int index){
+		
+		return !ships.get(index).isPlaced();
+	}
+	
+	/**
+	 * Checks to see if all ships has been placed
+	 * @return true if all ships are placed
+	 */
+	public boolean isPlaced(){
+		boolean isPlaced = true;
+		for(Ship s:ships)
+		{
+			if (isPlaced){
+				isPlaced = s.isPlaced();
+			}
+		}
+		return isPlaced;
+	}
+	
+	public Ship getShip(int index) {
+		return ships.get(index);
+	}
+
+	public void isSunk(){
+		boolean isSunk = true;
+		for (Ship s: ships){
+			if (isSunk){
+				isSunk = s.isSunk();
+			}
+		}
+		if (isSunk)
+			game.printToStatusField("You win!");		
+	}
+}
